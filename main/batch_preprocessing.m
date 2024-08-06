46cb47bd930fae61e174a30ac8dfe05139c9edea
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-08-06 13:06:32 +0200

    Add files via upload

---
+++ main/batch_preprocessing.m
@@ -0,0 +1,114 @@
+%% General stuff
+data_dir = 'C:\Users\berne\Documents\Master FU\2. Semester\NMDA practical\EEG\Multimodal\sub0';
+function_directory = 'C:\Users\berne\Documents\Master FU\2. Semester\NMDA practical\EEG\Multimodal\functions';
+
+
+subs = [14,15,16,17];
+
+%% Preprocessing - every run individually 
+
+% Convert
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    trial_dir = fullfile(strcat(data_dir,num2str(sub), '\Trials'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory);
+    % add path to function_directory 
+    for run = 1:length(runs) 
+
+        file = fullfile(strcat(file_dir,'\run_0',num2str(run),'_sss.fif'));
+        trial_file = fullfile(strcat(trial_dir,'\run_0',num2str(run),'_trldef.mat'));
+
+        A01_convert(file, trial_file)
+    end
+end
+
+
+% Prepare
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory);
+    % add path to function_directory 
+    for run = 1:length(runs) 
+        file = fullfile(strcat(file_dir,'\spmeeg_run_0',num2str(run),'_sss.mat'));
+        
+        A02_prepare(file)
+    end
+end
+
+% Prepare 2
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    for run = 1:length(runs) 
+        file = fullfile(strcat(file_dir,'\spmeeg_run_0',num2str(run),'_sss.mat'));
+        chan_file = fullfile(strcat(file_dir,'\bad_channels.mat'));
+        A03_prepare(file,chan_file)
+    end
+end
+
+% Downsample
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    for run = 1:length(runs) 
+        file = fullfile(strcat(file_dir,'\spmeeg_run_0',num2str(run),'_sss.mat'));
+        A04_downsample(file)
+    end
+end
+
+
+% Baseline Correction 
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    for run = 1:length(runs) 
+        file = fullfile(strcat(file_dir,'\dspmeeg_run_0',num2str(run),'_sss.mat'));
+        A05_bc(file)
+    end
+end
+
+%% Combine all runs 
+
+% Merging
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    A06_merging(file_dir, runs)
+end
+
+% Prepare
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\cbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    A07_prepare(file)
+end
+
+% Montaging
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file_1 = fullfile(strcat(file_dir,'\cbdspmeeg_run_01_sss.mat'));
+    file_2 = fullfile(strcat(file_dir,'\cbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    A08_montaging(file_1, file_2)
+end
---
+++ main/batch_time_frequency.m
@@ -0,0 +1,126 @@
+%% General stuff
+data_dir = 'C:\Users\berne\Documents\Master FU\2. Semester\NMDA practical\EEG\Multimodal\sub0';
+function_directory = 'C:\Users\berne\Documents\Master FU\2. Semester\NMDA practical\EEG\Multimodal\functions';
+
+subs = [14,15,16,17];
+
+%% Time Frequency analysis
+% Wavelet estimation
+%   .mat file which has not been cropped yet
+%   Output: two files with prefixes "tf" and "tph"
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\Mcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B01_wavelet(file)
+end
+
+%% Crop
+
+%   once for phase
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\tphMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B02_crop_phase(file)
+end
+
+%   once for power
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\tfMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B03_crop_power(file)
+end
+
+%% averaging
+
+%   once for power
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\ptphMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B04_average_phase(file)
+end
+
+%   once for phase for i = 1:length(subs)
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\ptfMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B05_average_power(file)
+end
+
+%% Rescale 
+
+%   once for power
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\mptfMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B06_rescale_power(file)
+end
+
+%% Averaging contrasts
+
+% once for phase
+    % specify contrasts
+contrasts = {};
+contrast_labels = {};
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\mpthfMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B07_averaging_contrast_phase(file, contrasts, contrast_labels)
+end
+
+% once for phase
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\rmptfMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B08_averaging_contrast_power(file, contrasts, contrast_labels)
+end
+
+%% Convert 2 images 
+
+%   Select baseline-rescaled, trial-averaged power file
+
+for sub = subs
+    file_dir = fullfile(strcat(data_dir,num2str(sub)));
+    file = fullfile(strcat(file_dir,'\wmptfMcbdspmeeg_run_01_sss.mat'));
+    % set working directory to subject 
+    cd(file_dir) 
+    addpath(function_directory); % add path to function_directory 
+    B09_convert2images(file)
+end
+
+%% Specification
+
+%   specify conditions in correct order
+%   create a loop for as many subjects as one has 
+
+C01_specification(data_dir, subs, conditions)
+
+%% estimation
+
+C02_estimation(output_dir)
