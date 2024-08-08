00fce5908a478e338762da5f3c50ac5855c3054b
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-08-08 11:34:08 +0200

    Update A07_prepare.m

--- functions/A07_prepare.m
+++ functions/A07_prepare.m
@@ -3,8 +3,12 @@ function A07_prepare(file)
 clear matlabbatch 
 
 matlabbatch{1}.spm.meeg.preproc.prepare.D(1) = {file};
-matlabbatch{1}.spm.meeg.preproc.prepare.task{1}.avref.fname = 'avref_montage.mat';
-
+matlabbatch{1}.spm.meeg.preproc.prepare.task{1}.sortconditions.label = {
+                                                                        'Famous'
+                                                                        'Unfamiliar'
+                                                                        'Scrambled'
+                                                                        }';
+matlabbatch{1}.spm.meeg.preproc.prepare.task{2}.avref.fname = 'avref_montage.mat';
 
 spm('defaults', 'EEG');
 spm_jobman('run', matlabbatch);
