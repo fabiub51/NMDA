a00e26a54a65ef277099017e0edecdde87a76b54
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-08-08 11:35:05 +0200

    Update B08_averaging_contrast_power.m

--- functions/B08_averaging_contrast_power.m
+++ functions/B08_averaging_contrast_power.m
@@ -1,8 +1,12 @@
-function B08_averaging_contrast_power(file, contrast, con_label)
+function B08_averaging_contrast_power(file)
 
 matlabbatch{1}.spm.meeg.averaging.contrast.D = {file};
-matlabbatch{1}.spm.meeg.averaging.contrast.contrast.c = contrast;
-matlabbatch{1}.spm.meeg.averaging.contrast.contrast.label = con_label;
+matlabbatch{1}.spm.meeg.averaging.contrast.contrast(1).c = [1 0 0];
+matlabbatch{1}.spm.meeg.averaging.contrast.contrast(1).label = 'Famous';
+matlabbatch{1}.spm.meeg.averaging.contrast.contrast(2).c = [0 1 0];
+matlabbatch{1}.spm.meeg.averaging.contrast.contrast(2).label = 'Unfamiliar';
+matlabbatch{1}.spm.meeg.averaging.contrast.contrast(3).c = [0 0 1];
+matlabbatch{1}.spm.meeg.averaging.contrast.contrast(3).label = 'Scrambled';
 matlabbatch{1}.spm.meeg.averaging.contrast.weighted = 1;
 matlabbatch{1}.spm.meeg.averaging.contrast.prefix = 'w';
 
